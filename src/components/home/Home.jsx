b552ec250a39db4fe7f8d892280365460ff0a97d
bopzen <boppp@abv.bg>
2024-09-08 12:28:48 +0300

    Updated Home component, added all stages of tournament

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,10 @@
 import GroupMatches from "../group-matches/GroupMatches";
+import MatchResult from "../match-result/MatchResult";
 
 export default function Home() {
     return (
         <>
+            <h2>Group Stage</h2>
             <div className="group-results-container">
                 <GroupMatches />
                 <GroupMatches />
@@ -12,6 +14,36 @@ export default function Home() {
                 <GroupMatches />
             </div>
             
+            <h2>Round of 16</h2>
+            <div className="round-of-16-container">
+                <MatchResult />
+                <MatchResult />
+                <MatchResult />
+                <MatchResult />
+                <MatchResult />
+                <MatchResult />
+                <MatchResult />
+                <MatchResult />
+            </div>
+            <h2>Quarter-finals</h2>
+            <div className="round-of-16-container">
+                <MatchResult />
+                <MatchResult />
+                <MatchResult />
+                <MatchResult />
+            </div>
+            <h2>Semi-finals</h2>
+            <div className="round-of-16-container">
+                <MatchResult />
+                <MatchResult />
+                <MatchResult />
+                <MatchResult />
+            </div>
+            <h2>Final</h2>
+            <div className="round-of-16-container">
+                <MatchResult />
+                <MatchResult />
+            </div>
         </>
     )
 }
