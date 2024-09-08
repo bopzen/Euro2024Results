b90619e23970856f809e2e61ac649fc10a9a2bd6
bopzen <boppp@abv.bg>
2024-09-08 20:51:40 +0300

    Updated and styled MatchDetails component

--- src/components/match-details/MatchDetails.jsx
+++ src/components/match-details/MatchDetails.jsx
@@ -1,7 +1,33 @@
 export default function MatchDetails() {
     return (
         <>
-            <h1>Match Details</h1>
+            <div className="match-details-section">
+                <div className="match-details-header">
+                    <h3>UEFA Euro 2024</h3>
+                    <h3>14 June 2024</h3>
+                    <h3>Full-time</h3>
+                </div>
+                <div className="match-details-result-big">
+                    <div className="team-container-big">
+                        <div className="country-flag-container-big">
+                            <img src="https://www.worldometers.info/img/flags/fr-flag.gif" alt="" className="country-flag-big" />
+                        </div>
+                        <p>France </p>
+                    </div>
+                    <div className="match-result-big">
+                        <p>1 : 2</p>
+                    </div>
+                    <div className="team-container-big">
+                        <div className="country-flag-container-big">
+                            <img src="https://www.worldometers.info/img/flags/sp-flag.gif" alt="" className="country-flag-big" />
+                        </div>
+                        <p><b>Spain</b></p>
+                    </div>              
+                </div>
+                <h4>Group A</h4>
+            </div>
+            
+            
         </>
     )
 }
