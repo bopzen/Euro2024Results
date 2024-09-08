db03e7c0433b54ad8251335a98f17f9b426bb87c
bopzen <boppp@abv.bg>
2024-09-08 22:02:16 +0300

    Designed team lineup field

--- src/components/team-lineup/TeamLineUp.jsx
+++ src/components/team-lineup/TeamLineUp.jsx
@@ -2,7 +2,12 @@ export default function TeamLineUp() {
     return (
         <>
             <div className="team-lineup-container">
-                
+                <div className="team-lineup-top-field">
+                </div>
+                <div className="team-lineup-bottom-field">
+                </div>
+                <div className="team-lineup-center">
+                </div>
             </div>
         </>
     )
