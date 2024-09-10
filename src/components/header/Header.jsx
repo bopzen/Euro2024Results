c64f6a96d20a87903616bc03609fa3657c989362
bopzen <boppp@abv.bg>
2024-09-10 23:14:26 +0300

    Updated Header and Footer

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,18 +4,12 @@ export default function Header() {
     return (
         <>
             <header>
-                <div className="euro-logo-container">
-                    <img src="/logos/ec-2024-general-logo.webp" alt="euro2024-logo" />
-                </div>
-                
+                <Link to="/">
+                    <div className="euro-logo-container">
+                        <img src="/logos/ec-2024-general-logo.webp" alt="euro2024-logo" />
+                    </div>
+                </Link>            
                 <h2>The UEFA European Football Championship 2024 Match Results</h2>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                        <Link to="/match">Match</Link>
-                        <Link to="/team">Team</Link>
-                    </li>
-                </ul>
             </header>
         </>
     )
