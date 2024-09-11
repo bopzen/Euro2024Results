66412566d217d9786d9b6de13c656fec806f49d5
bopzen <boppp@abv.bg>
2024-09-11 21:58:16 +0300

    Fixed issue with CSV parsing end of line splitter that was not working on Linux

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,7 +2,7 @@ export async function getDataFromCSV(filepath) {
     try {
         const response = await fetch(filepath);
         const data = await response.text();
-        const rows = data.split('\r\n');
+        const rows = data.split(/\r?\n/);
         return rows.map(row => row.split(','));
     } catch (error) {
         console.log(error)
