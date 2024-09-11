fb732bf3717ec75d17c869a9977a65baaf908192
bopzen <boppp@abv.bg>
2024-09-11 10:54:06 +0300

    Updated NotFound component

--- src/components/not-found/NotFound.jsx
+++ src/components/not-found/NotFound.jsx
@@ -1,7 +1,12 @@
 export default function NotFound() {
     return (
         <>
-            <h1>404 Page not found </h1>
+            <div className="not-found-contaienr">
+                <h1>404 Page not found </h1>
+                <div className="goal-image-container">
+                    <img src="/images/goal.jpg" alt="goal" />
+                </div>
+            </div>
         </>
     )
 }
