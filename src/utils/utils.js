bf8c573c3afe9ab996872170833bd1bf0e37522e
bopzen <boppp@abv.bg>
2024-09-11 21:44:27 +0300

    changed parsing splitter

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,7 +2,7 @@ export async function getDataFromCSV(filepath) {
     try {
         const response = await fetch(filepath);
         const data = await response.text();
-        const rows = data.split('\r\n');
+        const rows = data.split('\n');
         console.log('Fetched data: ' + rows);
         return rows.map(row => row.split(','));
     } catch (error) {
